0e6e892316c551770bfef4fac8d43fcd93af09de
JorgeGaro <h95jorgaro@gmail.com>
2026-02-24 19:56:32 +0100

    .

---
+++ RelojDigital/js/script.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+
+    //Llamamos a la función obtenerHora cada 500 milisegundos (medio segundo). Esto
+    //es lo que permite que el reloj se actualice
+    let t = setInterval(obtenerHora, 500);
+}
+
+function obtenerHora() {
+    let fecha = new Date(); //Creamos un objeto de tipo fecha
+
+    //Extraemos la hora de la variable donde hemos guardado la fecha
+    let h = fecha.getHours();
+    //Si la hora no tiene dos dígitos, le añadiremos un cero delante
+    if (h < 10) {
+        h = "0" + h;
+    }
+
+    //Extraemos los minutos de la variable donde hemos guardado la fecha
+    let m = fecha.getMinutes();
+    //Si los minutos no tienen dos dígitos, le añadiremos un cero delante
+    if (m < 10) {
+        m = "0" + m;
+    }
+
+    //Extraemos los segundos de la variable donde hemos guardado la fecha
+    let s = fecha.getSeconds();
+    //Si los segundos no tienen dos dígitos, le añadiremos un cero delante
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    document.getElementById("reloj").innerHTML = h + ":" + m + ":" + s;
+}
